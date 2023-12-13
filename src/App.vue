35106bd97ab0801b3c04cf7e6f2bfb5368b459b0
JouderMin <joudermin@foxmail.com>
2023-12-13 15:18:19 +0800

    init

---
+++ src/App.vue
@@ -0,0 +1,62 @@
+<template>
+  <div id="container">
+    <md-list id="select-list">
+      <md-list-item>
+        <h1 id="title" slot="headline">JouderMin的知识数据库</h1>
+      </md-list-item>
+      <md-divider />
+      <md-list-item type="button" @click="click" class="item-button">
+        <div slot="headline">Hello</div>
+        <div slot="supporting-text">World</div>
+      </md-list-item>
+      <md-divider />
+      <md-list-item type="button" @click="click" class="item-button">
+        <div slot="headline">新世纪计算机科学计划</div>
+        <div slot="supporting-text">New Age Computer Science Project</div>
+      </md-list-item>
+      <md-divider />
+      <md-list-item type="button" @click="click" class="item-button">
+        <div slot="headline">Hello</div>
+        <div slot="supporting-text">World</div>
+      </md-list-item>
+    </md-list>
+  </div>
+</template>
+
+<script setup lang="ts">
+import { MdList } from '@material/web/list/list';
+import { MdListItem } from '@material/web/list/list-item';
+import { MdDivider } from '@material/web/divider/divider';
+
+const click = () => {
+  window.location.href = 'https://www.baidu.com';
+};
+</script>
+
+<style scoped>
+#container {
+  width: fit-content;
+  height: fit-content;
+  padding: 20px;
+  padding-top: 0px;
+  background-color: var(--md-list-container-color, var(--md-sys-color-surface, #fef7ff));
+  border-radius: 8px;
+  text-rendering: geometricPrecision !important;
+}
+
+#select-list {
+  width: 500px;
+  max-width: 500px;
+  max-height: fit-content;
+}
+
+#title {
+  text-align: center;
+  line-height: 50px;
+  height: 50px;
+}
+
+.item-button {
+  border-radius: 8px;
+}
+</style>
